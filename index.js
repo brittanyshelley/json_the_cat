1a48ec7a00d0484c66eae47a9b86c4cc320d1193
brittanyshelley <brittany.j.shelley@gmail.com>
2023-11-23 22:19:38 -0700

    finished exercise

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const { fetchBreedDescription } = require('./breedFetcher');
 
 const breedName = process.argv[2];
 
-fetchBreedDescription(breedName, (error, description) => {
+fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
     console.log('Error fetch details:', error);
   } else {
-    console.log(description);
+    console.log(desc);
   }
 });
-
